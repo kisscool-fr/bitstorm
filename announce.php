c1f5291c5d497702bb6d86d81e7c971487a1ccbe
KisSCoOl <kisscool@kisscool.net>
2015-10-26 19:19:16 +0100

    Update to PDO, move configuration outside, make hex2bin compatible with PHP >= 5.4

--- announce.php
+++ announce.php
@@ -21,20 +21,17 @@
  ** Configuration start **
  *************************/
 
-//MySQL details
-define('__DB_SERVER', '');
-define('__DB_USERNAME', '');
-define('__DB_PASSWORD', '');
-define('__DB_DATABASE', '');
+// Configuration details
+require 'configuration.php';
 
-//Peer announce interval (Seconds)
+// Peer announce interval (Seconds)
 define('__INTERVAL', 1800);
 
-//Time out if peer is this late to re-announce (Seconds)
+// Time out if peer is this late to re-announce (Seconds)
 define('__TIMEOUT', 120);
 
-//Minimum announce interval (Seconds)
-//Most clients obey this, but not all
+// Minimum announce interval (Seconds)
+// Most clients obey this, but not all
 define('__INTERVAL_MIN', 60);
 
 // By default, never encode more than this number of peers in a single request
@@ -44,163 +41,243 @@ define('__MAX_PPR', 20);
  ** Configuration end **
  ***********************/
 
-//Use the correct content-type
+// Use the correct content-type
 header("Content-type: Text/Plain");
 
-//Connect to the MySQL server
-@mysql_connect(__DB_SERVER, __DB_USERNAME, __DB_PASSWORD) or die(track('Database connection failed'));
+// Connect to the MySQL server
+try
+{
+    $dbh = new PDO(
+        sprintf('mysql:dbname=%s;host=%s;port=%d', __DB_DATABASE, __DB_SERVER, __DB_PORT),
+        __DB_USERNAME,
+        __DB_PASSWORD,
+        array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_CASE => PDO::CASE_NATURAL,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        )
+    );
+} catch (PDOException $e) {
+    die(track('Database connection failed'));
+}
 
-//Select the database
-@mysql_select_db(__DB_DATABASE) or die(track('Unable to select database'));
-
-//Inputs that are needed, do not continue without these
+// Inputs that are needed, do not continue without these
 valdata('peer_id', true);
 valdata('port');
 valdata('info_hash', true);
 
-//Make sure we have something to use as a key
+// Make sure we have something to use as a key
 if (!isset($_GET['key'])) {
-	$_GET['key'] = '';
+    $_GET['key'] = '';
 }
 
 $downloaded = isset($_GET['uploaded']) ? intval($_GET['uploaded']) : 0;
 $uploaded = isset($_GET['uploaded']) ? intval($_GET['uploaded']) : 0;
 $left = isset($_GET['left']) ? intval($_GET['left']) : 0;
 
-//Validate key as well
+// Validate key as well
 valdata('key');
 
-//Do we have a valid client port?
+// Do we have a valid client port?
 if (!ctype_digit($_GET['port']) || $_GET['port'] < 1 || $_GET['port'] > 65535) {
-	die(track('Invalid client port'));
+    die(track('Invalid client port'));
 }
 
-//Hack to get comatibility with trackon
+// Hack to get comatibility with trackon
 if ($_GET['port'] == 999 && substr($_GET['peer_id'], 0, 10) == '-TO0001-XX') {
-	die("d8:completei0e10:incompletei0e8:intervali600e12:min intervali60e5:peersld2:ip12:72.14.194.184:port3:999ed2:ip11:72.14.194.14:port3:999ed2:ip12:72.14.194.654:port3:999eee");
+    die("d8:completei0e10:incompletei0e8:intervali600e12:min intervali60e5:peersld2:ip12:72.14.194.184:port3:999ed2:ip11:72.14.194.14:port3:999ed2:ip12:72.14.194.654:port3:999eee");
 }
 
-mysql_query('INSERT INTO `peer` (`hash`, `user_agent`, `ip_address`, `key`, `port`) '
-	. "VALUES ('" . mysql_real_escape_string(bin2hex($_GET['peer_id'])) . "', '" . mysql_real_escape_string(substr($_SERVER['HTTP_USER_AGENT'], 0, 80)) 
-	. "', INET_ATON('" . mysql_real_escape_string($_SERVER['REMOTE_ADDR']) . "'), '" . mysql_real_escape_string(sha1($_GET['key'])) . "', " . intval($_GET['port']) . ") "
-	. 'ON DUPLICATE KEY UPDATE `user_agent` = VALUES(`user_agent`), `ip_address` = VALUES(`ip_address`), `port` = VALUES(`port`), `id` = LAST_INSERT_ID(`peer`.`id`)') 
-	or die(track('Cannot update peer: '.mysql_error()));
-$pk_peer = mysql_insert_id();
+$count = $dbh->exec(
+    sprintf(
+        'INSERT INTO `peer` (`hash`, `user_agent`, `ip_address`, `key`, `port`) '
+        . 'VALUES (%s, %s, INET_ATON(%s), %s, %d) '
+        . 'ON DUPLICATE KEY UPDATE `user_agent` = VALUES(`user_agent`), `ip_address` = VALUES(`ip_address`), `port` = VALUES(`port`), `id` = LAST_INSERT_ID(`peer`.`id`)',
+        $dbh->quote(bin2hex($_GET['peer_id'])),
+        $dbh->quote(substr($_SERVER['HTTP_USER_AGENT'], 0, 80)),
+        $dbh->quote($_SERVER['REMOTE_ADDR']),
+        $dbh->quote(sha1($_GET['key'])),
+        intval($_GET['port'])
+    )
+);
 
-mysql_query("INSERT INTO `torrent` (`hash`) VALUES ('" . mysql_real_escape_string(bin2hex($_GET['info_hash'])) . "') "
- 	. "ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`)") or die(track('Cannot update torrent' . mysql_error())); // ON DUPLICATE KEY UPDATE is just to make mysql_insert_id work
-$pk_torrent = mysql_insert_id();
+if ($count === false) {
+    die(track('Cannot update peer: '.$dbh->errorInfo()[2]));
+}
 
-//User agent is required
+$pk_peer = $dbh->lastInsertId();
+
+$count = $dbh->exec(
+    sprintf(
+        'INSERT INTO `torrent` (`hash`) VALUES (%s) ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`)', // ON DUPLICATE KEY UPDATE is just to make mysql_insert_id work
+        $dbh->quote(bin2hex($_GET['info_hash']))
+    )
+);
+
+if ($count === false) {
+    die(track('Cannot update torrent: '.$dbh->errorInfo()[2]));
+}
+
+$pk_torrent = $dbh->lastInsertId();
+
+// User agent is required
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
-	$_SERVER['HTTP_USER_AGENT'] = "N/A";
+    $_SERVER['HTTP_USER_AGENT'] = "N/A";
 }
 if (!isset($_GET['uploaded'])) {
-	$_GET['uploaded'] = 0;
+    $_GET['uploaded'] = 0;
 }
 if (!isset($_GET['downloaded'])) {
-	$_GET['downloaded'] = 0;
+    $_GET['downloaded'] = 0;
 }
 if (!isset($_GET['left'])) {
-	$_GET['left'] = 0;
+    $_GET['left'] = 0;
 }
 
-mysql_query('INSERT INTO `peer_torrent` (`peer_id`, `torrent_id`, `uploaded`, `downloaded`, `left`, `last_updated`) '
-	. 'SELECT ' . $pk_peer . ', `torrent`.`id`, ' . intval($_GET['uploaded']) . ', ' . intval($_GET['downloaded']) . ', ' . intval($_GET['left']) . ', UTC_TIMESTAMP() '
-	. 'FROM `torrent` '
-	. "WHERE `torrent`.`hash` = '" . mysql_real_escape_string(bin2hex($_GET['info_hash'])) . "' "
-	. 'ON DUPLICATE KEY UPDATE `uploaded` = VALUES(`uploaded`), `downloaded` = VALUES(`downloaded`), `left` = VALUES(`left`), `last_updated` = VALUES(`last_updated`), '
-	. '`id` = LAST_INSERT_ID(`peer_torrent`.`id`)')
-	or die(track(mysql_error()));
-$pk_peer_torrent = mysql_insert_id();
 
-//Did the client stop the torrent?
+$count = $dbh->exec(
+    sprintf(
+        'INSERT INTO `peer_torrent` (`peer_id`, `torrent_id`, `uploaded`, `downloaded`, `left`, `last_updated`) '
+        . 'SELECT %d, `torrent`.`id`, %d, %d, %d, UTC_TIMESTAMP() '
+        . 'FROM `torrent` '
+        . "WHERE `torrent`.`hash` = %s "
+        . 'ON DUPLICATE KEY UPDATE `uploaded` = VALUES(`uploaded`), `downloaded` = VALUES(`downloaded`), `left` = VALUES(`left`), `last_updated` = VALUES(`last_updated`), '
+        . '`id` = LAST_INSERT_ID(`peer_torrent`.`id`)',
+        $pk_peer,
+        intval($_GET['uploaded']),
+        intval($_GET['downloaded']),
+        intval($_GET['left']),
+        $dbh->quote(bin2hex($_GET['info_hash']))
+    )
+);
+
+if ($count === false) {
+    die(track($dbh->errorInfo()[2]));
+}
+
+$pk_peer_torrent = $dbh->lastInsertId();
+
+// Did the client stop the torrent?
 if (isset($_GET['event']) && $_GET['event'] === 'stopped') {
-	mysql_query("UPDATE `peer_torrent` SET `stopped` = TRUE WHERE `id` = " . $pk_peer_torrent) or die (track(mysql_error()));
-	die(track(array(), 0, 0)); //The RFC says its OK to return an empty string when stopping a torrent however some clients will whine about it so we return an empty dictionary
+    $count = $dbh->exec(
+        sprintf(
+            'UPDATE `peer_torrent` SET `stopped` = TRUE WHERE `id` = %s',
+            $dbh->quote($pk_peer_torrent, PDO::PARAM_INT)
+        )
+    );
+
+    if ($count === false) {
+        die(track($dbh->errorInfo()[2]));
+    }
+
+    die(track(array(), 0, 0)); //The RFC says its OK to return an empty string when stopping a torrent however some clients will whine about it so we return an empty dictionary
 }
 
 $numwant = __MAX_PPR; //Can be modified by client
 
-//Set number of peers to return
+// Set number of peers to return
 if (isset($_GET['numwant']) && ctype_digit($_GET['numwant']) && $_GET['numwant'] <= __MAX_PPR && $_GET['numwant'] >= 0) {
-	$numwant = (int)$_GET['numwant'];
+    $numwant = intval($_GET['numwant']);
 }
 
-$q = mysql_query('SELECT INET_NTOA(peer.ip_address), peer.port, peer.hash '
-	. 'FROM peer_torrent '
-	. 'JOIN peer ON peer.id = peer_torrent.peer_id '
-	. 'WHERE peer_torrent.torrent_id = ' . $pk_torrent . ' AND peer_torrent.stopped = FALSE '
-	. 'AND peer_torrent.last_updated >= DATE_SUB(UTC_TIMESTAMP(), INTERVAL ' . (__INTERVAL + __TIMEOUT) . ' SECOND) '
-	. 'AND peer.id != ' . $pk_peer . ' '
-	. 'ORDER BY RAND() '
-	. 'LIMIT ' . $numwant) or die(track(mysql_error()));
+$results = $dbh->query(
+    sprintf(
+        'SELECT INET_NTOA(peer.ip_address), peer.port, peer.hash '
+        . 'FROM peer_torrent '
+        . 'JOIN peer ON peer.id = peer_torrent.peer_id '
+        . 'WHERE peer_torrent.torrent_id = %d AND peer_torrent.stopped = FALSE '
+        . 'AND peer_torrent.last_updated >= DATE_SUB(UTC_TIMESTAMP(), INTERVAL %d SECOND) '
+        . 'AND peer.id != %d '
+        . 'ORDER BY RAND() '
+        . 'LIMIT %d',
+        $pk_torrent,
+        (__INTERVAL + __TIMEOUT),
+        $pk_peer,
+        $numwant
+    )
+);
+
+if ($results === false) {
+    die(track($dbh->errorInfo()[2]));
+}
 
 $reply = array(); //To be encoded and sent to the client
 
-while ($r = mysql_fetch_array($q)) { //Runs for every client with the same infohash
-	$reply[] = array($r[0], $r[1], $r[2]); //ip, port, peerid
+while ($r = $results->fetch(PDO::FETCH_NUM)) { //Runs for every client with the same infohash
+    $reply[] = array($r[0], $r[1], $r[2]); //ip, port, peerid
 }
 
-$q = mysql_query('SELECT IFNULL(SUM(peer_torrent.left > 0), 0) AS leech, IFNULL(SUM(peer_torrent.left = 0), 0) AS seed '
-	. 'FROM peer_torrent '
-	. 'WHERE peer_torrent.torrent_id = ' . $pk_torrent . ' AND `peer_torrent`.`stopped` = FALSE '
-	. 'AND peer_torrent.last_updated >= DATE_SUB(UTC_TIMESTAMP(), INTERVAL ' . (__INTERVAL + __TIMEOUT) . ' SECOND) '
-	. 'GROUP BY `peer_torrent`.`torrent_id`') or die(track(mysql_error()));
+$results = $dbh->query(
+    sprintf(
+        'SELECT IFNULL(SUM(peer_torrent.left > 0), 0) AS leech, IFNULL(SUM(peer_torrent.left = 0), 0) AS seed '
+        . 'FROM peer_torrent '
+        . 'WHERE peer_torrent.torrent_id = %d AND `peer_torrent`.`stopped` = FALSE '
+        . 'AND peer_torrent.last_updated >= DATE_SUB(UTC_TIMESTAMP(), INTERVAL %d SECOND) '
+        . 'GROUP BY `peer_torrent`.`torrent_id`',
+        $pk_torrent,
+        (__INTERVAL + __TIMEOUT)
+    )
+);
+
+if ($results === false) {
+    die(track($dbh->errorInfo()[2]));
+}
 
 $seeders = 0;
 $leechers = 0;
 
-if ($r = mysql_fetch_array($q))
+if ($r = $results->fetch(PDO::FETCH_NUM))
 {
-	$seeders = $r[1];
-	$leechers = $r[0];
+    $seeders = $r[1];
+    $leechers = $r[0];
 }
 
 die(track($reply, $seeders[0], $leechers[0]));
 
-//Bencoding function, returns a bencoded dictionary
-//You may go ahead and enter custom keys in the dictionary in
-//this function if you'd like.
+// Bencoding function, returns a bencoded dictionary
+// You may go ahead and enter custom keys in the dictionary in
+// this function if you'd like.
 function track($list, $c=0, $i=0) {
-	if (is_string($list)) { //Did we get a string? Return an error to the client
-		return 'd14:failure reason'.strlen($list).':'.$list.'e';
-	}
-	$p = ''; //Peer directory
-	foreach($list as $d) { //Runs for each client
-		$pid = '';
-		if (!isset($_GET['no_peer_id'])) { //Send out peer_ids in the reply
-			$real_id = hex2bin($d[2]);
-			$pid = '7:peer id'.strlen($real_id).':'.$real_id;
-		}
-		$p .= 'd2:ip'.strlen($d[0]).':'.$d[0].$pid.'4:porti'.$d[1].'ee';
-	}
-	//Add some other paramters in the dictionary and merge with peer list
-	$r = 'd8:intervali'.__INTERVAL.'e12:min intervali'.__INTERVAL_MIN.'e8:completei'.$c.'e10:incompletei'.$i.'e5:peersl'.$p.'ee';
-	return $r;
+    if (is_string($list)) { //Did we get a string? Return an error to the client
+        return 'd14:failure reason'.strlen($list).':'.$list.'e';
+    }
+    $p = ''; // Peer directory
+    foreach($list as $d) { // Runs for each client
+        $pid = '';
+        if (!isset($_GET['no_peer_id'])) { // Send out peer_ids in the reply
+            $real_id = hex2bin($d[2]);
+            $pid = '7:peer id'.strlen($real_id).':'.$real_id;
+        }
+        $p .= 'd2:ip'.strlen($d[0]).':'.$d[0].$pid.'4:porti'.$d[1].'ee';
+    }
+    // Add some other paramters in the dictionary and merge with peer list
+    $r = 'd8:intervali'.__INTERVAL.'e12:min intervali'.__INTERVAL_MIN.'e8:completei'.$c.'e10:incompletei'.$i.'e5:peersl'.$p.'ee';
+    return $r;
 }
 
-//Do some input validation
+// Do some input validation
 function valdata($g, $fixed_size=false) {
-	if (!isset($_GET[$g])) {
-		die(track('Invalid request, missing data'));
-	}
-	if (!is_string($_GET[$g])) {
-		die(track('Invalid request, unknown data type'));
-	}
-	if ($fixed_size && strlen($_GET[$g]) != 20) {
-		die(track('Invalid request, length on fixed argument not correct'));
-	}
-	if (strlen($_GET[$g]) > 80) { //128 chars should really be enough
-		die(track('Request too long'));
-	}
+    if (!isset($_GET[$g])) {
+        die(track('Invalid request, missing data'));
+    }
+    if (!is_string($_GET[$g])) {
+        die(track('Invalid request, unknown data type'));
+    }
+    if ($fixed_size && strlen($_GET[$g]) != 20) {
+        die(track('Invalid request, length on fixed argument not correct'));
+    }
+    if (strlen($_GET[$g]) > 80) { // 128 chars should really be enough
+        die(track('Request too long'));
+    }
 }
 
-function hex2bin($hex) {
-	$r = '';
-	for ($i=0; $i < strlen($hex); $i+=2) {
-		$r .= chr(hexdec($hex{$i}.$hex{($i+1)}));
-	}
-	return $r;
+if (!function_exists('hex2bin')) {
+    function hex2bin($hex) {
+        $r = '';
+        for ($i=0; $i < strlen($hex); $i+=2) {
+            $r .= chr(hexdec($hex{$i}.$hex{($i+1)}));
+        }
+        return $r;
+    }
 }
 ?>
